fa72597ab3e5fe3bb000937e0f15250c908f2b6c
Silvio Lucena Junior <silvio.lucena.junior@intrapoint.com>
2015-04-22 17:24:31 -0300

    some improves

--- js/board.js
+++ js/board.js
@@ -1,4 +1,12 @@
 var FREE = 0;
+var STARTBOARD = {
+  'blackpieces' : {
+    'paws' : ["A7", "B7", "C7", "D7", "E7", "F7", "G7", "H7"]
+  },
+  'whitepieces' : {
+    'paws' : ["A2", "B2", "C2", "D2", "E2", "F2", "G2", "H2"]
+  }
+};
 function Board(containerID){
    this._board = null;
    this._containerID = containerID;
@@ -24,14 +32,28 @@ Board.prototype.renderHTMLBoard = function RenderHTMLBoard(){
 	for (var i = 1; i <= 8; i++) {
   	   this._board[i] = [];
        for (var j = 1; j <= 8; j++) {
+          var id = String.fromCharCode(64 + j) + (9 - i);
        	  if ((i + j) % 2 === 0){
        	  	var square_class = 'white';
        	  } else {
        	  	var square_class = 'black';
        	  }
-          _html += "<span class='square " + square_class + "'></span>";
+          _html += "<span id='" + id +"' class='square " + square_class + "'></span>";
        }
     }
     _html += "</div>";
     document.getElementById(this._containerID).innerHTML = _html;
 };
+
+Board.prototype.addPiecesBox = function AddPiecesBox(){
+   
+}
+
+Board.prototype.setToStartPosition = function setToStartPosition(){
+  for (var i = 0; i < 8; i++){
+      $("#" + STARTBOARD.whitepieces.paws[i]).append("<span class='classical_piece pawn_white'></span>");
+      $("#" + STARTBOARD.blackpieces.paws[i]).append("<span class='classical_piece pawn_black'></span>");
+  } 
+}
+
+
