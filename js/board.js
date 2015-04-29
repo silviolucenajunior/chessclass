cc3f6c6144e611fc3e91c806819efda714749ea1
silviolucenajunior <silvio_lucena_junior@hotmail.com>
2015-04-28 23:28:23 -0300

    Some changes to use chessboard.js

--- js/board.js
+++ js/board.js
@@ -1,10 +1,12 @@
 var FREE = 0;
 var STARTBOARD = {
   'blackpieces' : {
-    'paws' : ["A7", "B7", "C7", "D7", "E7", "F7", "G7", "H7"]
+    'paws' : ["A7", "B7", "C7", "D7", "E7", "F7", "G7", "H7"],
+    'pieces' : ["TA8", "TH8", "CB8", "CG8", "BC8", "BF8", "DD8", "RE8"]
   },
   'whitepieces' : {
-    'paws' : ["A2", "B2", "C2", "D2", "E2", "F2", "G2", "H2"]
+    'paws' : ["A2", "B2", "C2", "D2", "E2", "F2", "G2", "H2"],
+    'pieces' : ["TA1", "TH1", "CB1", "CG1", "BC1", "BF1", "DD1", "RE1"]
   }
 };
 function Board(containerID){
@@ -51,9 +53,72 @@ Board.prototype.addPiecesBox = function AddPiecesBox(){
 
 Board.prototype.setToStartPosition = function setToStartPosition(){
   for (var i = 0; i < 8; i++){
-      $("#" + STARTBOARD.whitepieces.paws[i]).append("<span class='classical_piece pawn_white'></span>");
-      $("#" + STARTBOARD.blackpieces.paws[i]).append("<span class='classical_piece pawn_black'></span>");
+      $("#" + STARTBOARD.whitepieces.paws[i]).append("<span class='piece classical_piece pawn_white'></span>");
+      $("#" + STARTBOARD.blackpieces.paws[i]).append("<span class='piece classical_piece pawn_black'></span>");
+
+      var piece_class = '';
+      switch (STARTBOARD.whitepieces.pieces[i].substr(0, 1)) {
+        case 'T' : {
+           piece_class = 'rock_white';  
+           break;
+        }
+        case 'B' : {
+           piece_class = 'bishop_white';  
+           break;
+        }
+        case 'C' : {
+           piece_class = 'knight_white';  
+           break;
+        }
+        case 'D' : {
+           piece_class = 'queen_white';  
+           break;
+        }
+        case 'R' : {
+           piece_class = 'king_white';  
+           break;
+        }
+      }
+
+      $("#" + STARTBOARD.whitepieces.pieces[i].substr(1, 3)).append("<span class='piece classical_piece " + piece_class + "'></span>");
+     
+     switch (STARTBOARD.blackpieces.pieces[i].substr(0, 1)) {
+        case 'T' : {
+           piece_class = 'rock_black';  
+           break;
+        }
+        case 'B' : {
+           piece_class = 'bishop_black';  
+           break;
+        }
+        case 'C' : {
+           piece_class = 'knight_black';  
+           break;
+        }
+        case 'D' : {
+           piece_class = 'queen_black';  
+           break;
+        }
+        case 'R' : {
+           piece_class = 'king_black';  
+           break;
+        }
+      }
+
+      $("#" + STARTBOARD.blackpieces.pieces[i].substr(1, 3)).append("<span class='piece classical_piece " + piece_class + "'></span>");
   } 
 }
+
+Board.prototype.initGUIEvents = function initGUIEvents(){
+  $(".piece").draggable();
+
+  $(".square").droppable({
+     drop: function(event, ui){
+        console.log("DROPED") ;
+     }
+  });
+};
+
+
 
 
