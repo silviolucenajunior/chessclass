33939696217c5ac60f3dd30f721e2cc4514e70d3
Silvio Lucena Junior <silvio.lucena.junior@intrapoint.com>
2015-05-12 19:07:54 -0300

    add nodejs

---
+++ server/app.js
@@ -0,0 +1,21 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.writeHead(200, {
+      'Content-Type': 'text/html'
+   });
+   res.end('Congratulations, this page is the socket server running!');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('move', function(msg){
+    console.log('move: ' + msg);
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
